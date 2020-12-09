5b96ae41932028831cf81aab61d95f840970beef
Matheus Lopes <matheus_barbosa5@hotmail.com>
2020-12-09 02:32:18 -0300

    Rota de detalhes adicionada

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 import Home from "~/screens/Home";
 import NotFound from "~/screens/NotFound";
+import FilmDetails from "~/screens/Films/Details";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/filme/:id" component={FilmDetails} />
       <Route path="*" component={NotFound} />
     </Switch>
   );
